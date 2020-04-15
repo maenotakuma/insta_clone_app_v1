206e024e1fcc74a26ae91908909ce9e3f74bb11b
maenotakuma <front858@gmail.com>
2020-04-15 14:05:45 +0900

    いいね機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,6 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
 
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
 end
