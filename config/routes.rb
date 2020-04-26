082d73c132c78a63c12863fec3cec56381340385
maenotakuma <front858@gmail.com>
2020-04-26 18:29:38 +0900

    [Add]検索用のroutes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[index new create show]
   resources :posts, shallow: true do
+    collection do
+      get :search
+    end
     resources :comments
   end
   resources :likes, only: %i[create destroy]
