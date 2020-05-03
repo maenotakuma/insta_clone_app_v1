ecb1ba0ac3b21b5b22ede372c37e93bb62cc5045
maenotakuma <front858@gmail.com>
2020-05-03 10:34:56 +0900

    [Add]アバター用のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_081326) do
+ActiveRecord::Schema.define(version: 2020_05_03_004100) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_081326) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
