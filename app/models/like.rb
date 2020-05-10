deadeef9f10ced16f5cd0b7fd5e7fa67b231f32b
maenotakuma <front858@gmail.com>
2020-05-10 12:29:42 +0900

    [Add]フォローを外すとアクティビティの先のリレーションがなくなって落ちるバグ修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
+  has_one :activity, as: :subject, dependent: :destroy
+  
   validates :user_id, uniqueness: { scope: :post_id }
 
   after_create_commit :create_activities
